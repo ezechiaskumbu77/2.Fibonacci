8487531f670ef8011930cdb392eeb0ac1b4ced91
Kumbu Kumbu Ezechias <54143463+ezechiaskumbu77@users.noreply.github.com>
2020-06-19 13:21:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var output = [];
+var n;
+
+function fibonacciGenerator(n) {
+    for (var i = 1; i < n; n++) {
+        n = n + (n - 1);
+        if (n === 0) {
+            console.log(output.push[0]);
+        } else if (n === 1) {
+            cosole.log(output.push[0]);
+        } else
+            console.log(output.push[n]);
+        console.log(output);
+    }
+}
